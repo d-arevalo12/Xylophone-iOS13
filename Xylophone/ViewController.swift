536a4896c3f19e48abb61f4748dbcf7e30362893
d-arevalo12 <diego.arevalo@improving.com>
2024-08-30 22:28:10 -0600

    Added key functionality and sound when pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+  var player: AVAudioPlayer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 
+  @IBAction func keyCPressed(_ sender: UIButton) {
+    playSound(keyName: "C")
+  }
 
-    
+  @IBAction func keyEPressed(_ sender: UIButton) {
+    playSound(keyName: "E")
+  }
 
+  @IBAction func keyDPressed(_ sender: UIButton) {
+    playSound(keyName: "D")
+  }
+
+  @IBAction func keyFPressed(_ sender: UIButton) {
+    playSound(keyName: "F")
+  }
+
+  @IBAction func keyGPressed(_ sender: UIButton) {
+    playSound(keyName: "G")
+  }
+
+  @IBAction func keyAPressed(_ sender: UIButton) {
+    playSound(keyName: "A")
+  }
+
+  @IBAction func keyBPressed(_ sender: UIButton) {
+    playSound(keyName: "B")
+  }
+
+  func playSound(keyName: String) {
+    let url = Bundle.main.url(forResource: keyName, withExtension: "wav")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
+  }
 }
 
